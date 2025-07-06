2ffcd856d81ffda89c4ef41774d1ba444c6f8067
aayush23091 <aaayushyadav0@gmail.com>
2025-07-06 16:49:52 +0545

    header and footer updated

---
+++ retrocrug-sports/src/Footer.jsx
@@ -0,0 +1,28 @@
+// src/components/Footer.jsx
+import React from 'react';
+import './Footer.css'; // create this to move footer styles
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="footerfirst">
+        <img src="/retrocruglogo.png" alt="logo" />
+        <br />
+        Copyright © 2025 - All rights reserved.
+      </div>
+      <div className="footersecond">
+        <h3>Contact Us</h3>
+        <p>Email: contact@retrocrug.com</p>
+        <p>Phone: +977-9815855279</p>
+        <p>Address: Koteswor, Kathmandu, Nepal</p>
+      </div>
+      <div className="footerthird">
+        <h3>Follow us</h3>
+        <a href="/"><img src="/instalogo.png" alt="Instagram" /></a>
+        <a href="/"><img src="/twitterlogo.png" alt="Twitter" /></a>
+        <a href="/"><img src="/facebook.png" alt="Facebook" /></a>
+        <a href="/"><img src="/githublogo.png" alt="GitHub" /></a>
+      </div>
+    </footer>
+  );
+}
---
+++ retrocrug-sports/src/Header.jsx
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'; 
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <div className="nav-left">
+          <div className="logo">
+            <img className="retro" src="/retrocruglogo.png" alt="logo" />
+          </div>
+        </div>
+        <div className="nav-right">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/products">Products</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
